38d688b43c532b3503b8007706fe59fbea01f7e0
StephanieLuciaM <marie_lucia@hotmail.fr>
2025-02-10 12:57:33 +0100

    first commit

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
 
 // Lancer un serveur
 const PORT = process.env.PORT || 3000; // Valeur de rattrapage (fallback) si process.env.PORT === undefined, on lancera par défaut sur le port 3000
 app.listen(PORT, () => {
-    console.log(`🚀 Server listening at http://localhost:${PORT}`);
+  console.log(`🚀 Server listening at http://localhost:${PORT}`);
 });
