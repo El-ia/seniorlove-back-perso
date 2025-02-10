736aaa9da8bd54e140f5f46284789a9715a433e6
StephanieLuciaM <marie_lucia@hotmail.fr>
2025-02-10 16:22:10 +0100

    initial index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import "dotenv/config";
-import express, { Router } from "express";
-import { router } from "./src/routes/index.routes.js";
+import express from "express";
+
 
 const app = express();
 
-// Configure the body parser (to retrieve form data)
-app.use(express.urlencoded({ extended: true }));
 
-// BodyParser allowing to interpret data provided in a POST, PATCH, or PUT request as JSON. This data will be stored in req.body
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
-app.use(router);
 
-// Start a server
-const PORT = process.env.PORT || 3000; // Fallback value if process.env.PORT === undefined, the server will default to port 3000
+// Lancer un serveur
+const PORT = process.env.PORT || 3000; // Valeur de rattrapage (fallback) si process.env.PORT === undefined, on lancera par défaut sur le port 3000
 app.listen(PORT, () => {
   console.log(`🚀 Server listening at http://localhost:${PORT}`);
 });
