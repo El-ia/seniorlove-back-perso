a120090ba627b81c99173200ed420d6df4b34f9b
LudovicCazenave <lcazenav@gmail.com>
2025-02-15 15:15:18 +0100

    authentification done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import { router } from "./src/routes/index.routes.js";
 import cors from "cors"; // Import cors
+import cookieParser from "cookie-parser";
 
 import { jwtMiddleware } from "./src/middlewares/jwtMiddleware.js"; // Import JWT middleware
 
@@ -10,7 +11,9 @@ const app = express();
 // CORS module: specify who can access the API
 app.use(cors({
   origin: "http://localhost:5173", // * = allow everyone (not a best practice, but it's fine for local development),
-  credentials: true
+  credentials: true,
+  methods: "GET, PUT, POST, DELETE",
+  allowedHeaders: "Content-Type, Authorization"
 }));
 
 // Add JWT middleware to add user to req if token exists and is valid
@@ -18,7 +21,7 @@ app.use(cors({
 
 // Error handling middleware
 // app.use(errorHandler);
-
+app.use(cookieParser());
 // Body parser configuration (to retrieve form data)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Allow interpreting data provided in a POST, PATCH, or PUT request as JSON
