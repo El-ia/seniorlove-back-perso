1eb34313ff93d1127a9df2d4a79b3d4cfefd01e4
Elia Berthier <eliaberthier@MacBook-Air-de-Elia.local>
2025-03-09 14:06:42 +0100

    change cors render

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(helmet());
 
 // CORS module: specify who can access the API
 app.use(cors({
-  origin: "senior-love-front-3ypf.vercel.app", // * = allow everyone (not a best practice, but it's fine for local development),
+  origin: "https://senior-love-front-3ypf.vercel.app", // * = allow everyone (not a best practice, but it's fine for local development),
   credentials: true,
   methods: "GET, PUT, POST, PATCH, DELETE",
   allowedHeaders: "Content-Type, Authorization"
