8928243028a9c2ec311f942b481b9b48d819446e
Elia Berthier <eliaberthier@MacBook-Air-de-Elia.local>
2025-03-09 12:19:26 +0100

    SSL

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 //init sequelize connexion with our BDD
 export const sequelize = new Sequelize(
   process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: isProduction
+      ? { ssl: { require: true, rejectUnauthorized: false } } // En prod, Render exige SSL
+      : {}, // En local, pas besoin de SSL
     define: {
       createdAt: "created_at",
       updatedAt: "updated_at",
